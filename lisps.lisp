efc7907d3d8820720455a1aec7a48bf0601b5331
Tom Gillespie <tgbugs@gmail.com>
2015-08-27 18:40:49 -0700

    we MUST develop good notation for various none-one-many transitions that exist for functions in the real world, modeling sexual reproduction seems like a really tough test case, if we can do that, we should be able to do most things

--- lisps.lisp
+++ lisps.lisp
@@ -17,6 +17,28 @@
   ; desired: (learn_english human) -> (learn_english human)
   `(defun ,name (,@args) (list ',name ,@args)))
 
+(defmacro Z (f x &body body)
+  `(defun ,f (,@x) (',f ,@x)))
+
+((lambda (name) (lambda (args))))
+
+((lambda (binding-function)
+  (lambda (environment)
+    (lambda (identifier)
+      (lambda (expression)
+        (binding-function environment identifier expression)))))
+ (bind)(namespace-dict)(name)(some-other-expression))
+
+((lambda (binding-function)
+  (lambda (environment)
+    (lambda (identifier)
+      (lambda (expression)
+        (binding-function environment identifier expression))
+      (some-other-expression))
+    (name))
+  (namespace-dict))
+(bind))
+  
 (defparameter human 'human)  ; this is what we want maprw to do
 
 (defrwfun learn_english (human)
@@ -33,6 +55,41 @@
 ;   (realworld digital) -> realworld
 ;   (realworld digital) -> digital
 ;   (realworld digital) -> (realworld digital)
+
+; THE ABOVE IS FALSE:
+; there are 9 types that we need to deal with for
+; state modifying functions! we also need to implement
+; the full set of functions for being transititions
+;   nothing -> being (probably "named form" or something)
+;   being -> being (realworld -> realworld)
+;   n beings -> m beings (sexual reproduction over time?)
+;   being -> n beings (binary fision, budding)
+;   n beings -> being (slime mold fusion, sperm/egg)
+;   being -> other being (death, ceasing to incorporate matter)
+;   being -> nothing (destruction, dispersal, reincorporation)
+; all of these types of functions need to have different
+; output types because (learn-english human) is not universal
+; one ought to wonder whether (sex man woman) should evaluate
+; to ((sex man woman) child) or maybe
+; (sex man woman) -> (maybe (pregnant woman)))
+; (wait (pregnant woman))
+; written more clearly:
+; [male (range 5 60)mins](defun impregnate (female) ("Woo!"))
+; (impregnate female) -> (maybe (impregnate female))
+; [universe](defun wait (time subset-of-uniververse)("Time slowly passes..."))
+; (visual-inspection-for-signs-of-pregnanacy (wait (months 5) (maybe (impregnate female)))) -> (impregnate female)
+; better version: [executor](defun check (maybe-state-thing) (cond ((maybe) maybe-state-thing) ((yes) state-thing) ((no) thing)))
+
+(defun labor-pregnancy (pregnant-female)
+  (
+  )
+[developmental-machinery](defun gestation (pregnant-female)
+	    (offspring)) ; where does the description go here?
+; this is what we need to deal with because the syntax needs to be clean for specifying the processes involved in gestation
+; and that end in live birth, or a 'live born offspring'
+
+(gestation (impregnate female)) ; -> offspring (m or f)
+
 
 (defmacro defcombout (name args &body body)
   ("data collection code goes here!") ; or maybe... elsehwere?
