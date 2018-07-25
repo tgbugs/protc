90b7a5ed39f01faefc43643220c2624e1abac7e3
Tom Gillespie <tgbugs@gmail.com>
2018-07-24 21:36:49 -0700

    skeleton for protc export cli and renamed protcheck to check despite the great amusment provided by protc heck

--- protc-tools-lib/protc/info.rkt
+++ protc-tools-lib/protc/info.rkt
@@ -1,7 +1,11 @@
 #lang info
 
 (define raco-commands
-  (list (list "check-protocol"
-              'protc/protcheck/cli
+  (list (list "protc-check"
+              'protc/check/cli
               "runs correctness and completeness checks for Protc protocols"
+              #f)
+        (list "protc-export"  ; FIXME want protc export, protc check etc...
+              'protc/export/cli
+              "export Protc protocols to executable formats"
               #f)))
