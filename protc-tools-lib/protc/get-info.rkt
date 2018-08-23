783917cc9d0aab1a7d4da642b43ae67aaad7aa49
Tom Gillespie <tgbugs@gmail.com>
2018-08-23 02:03:40 -0700

    attempting to debug why color-lexer is not working

--- protc-tools-lib/protc/get-info.rkt
+++ protc-tools-lib/protc/get-info.rkt
@@ -4,10 +4,19 @@
 
 (define (protc/base-get-info key default default-filter)
   (case key
-    [(color-lexer) (dynamic-require 'protc/base/color 'protc-color)]
+    [(color-lexer) (dynamic-require 'protc/private/color 'protc-color)]
     ;[(drracket:indentation) (dynamic-require 'protc/base/indenter 'indent)]
     ;[(drracket:indentation) (indent)]
     [(drracket:toolbar-buttons) (dynamic-require 'protc/protcheck 'button-list)]
     [(drracket:default-filters) '(["Protc Sources" "*.ptc"])]
     [(drracket:default-extension) "rkt"]
     [else (default-filter key default)]))
+
+(module+ test
+  ((dynamic-require 'protc/private/color 'protc-color) (open-input-string "hahahhah") #;0)
+  (define color (protc/base-get-info 'color-lexer (λ (p o r) "heh") #t))
+  (color (open-input-string "a") #;0)
+  (color (open-input-string "impl") #;0)
+  (color (open-input-string "impl ") #;0)
+  (color (open-input-string "impl\n") #;0)
+  )
--- protc-tools-lib/protc/private/color.rkt
+++ protc-tools-lib/protc/private/color.rkt
@@ -1,46 +1,100 @@
 #lang racket/base
-(require brag/support syntax-color/racket-lexer)
+(require racket/string brag/support syntax-color/racket-lexer)
 (provide protc-color)
 
 (define-lex-abbrev section (:or "spec" "impl"))
 (define-lex-abbrev activity (:or "measure" "actualize" "make"))
 
+(define (protc-color port offset mode)
+  (define-values (line col pos) (port-next-location port))
+  (define c (read-char port))
+  (cond
+    [(eof-object? c)
+     (values c 'eof #f #f #f 0 mode)]
+    [else
+     (values (string c)
+             (if mode 'error 'error)
+             #f
+             (+ pos)
+             (+ pos 1)
+             0
+             (not mode))]))
+
 (define protc-lexer
   (lexer
    [(eof) (values lexeme 'eof #f #f #f)]
    [section (values lexeme 'keyword lexeme (pos lexeme-start) (pos lexeme-end))]
-   [activity (values lexeme 'error lexeme (pos lexeme-start) (pos lexeme-end))]
+   [activity (values lexeme 'keyword lexeme (pos lexeme-start) (pos lexeme-end))]
    #;
-   [any-char (values lexeme 'string #f (pos lexeme-start) (pos lexeme-end))]))
+   [any-char (values lexeme 'error #f (pos lexeme-start) (pos lexeme-end))]))
 
-(define (protc-color port offset racket-coloring-mode?)
+#;
+(define plog (make-logger 'protc-color #f #;'drracket-language 'info))
+
+#;
+(define (protc-color in)
+  (log-message plog 'info "currently inside protc-color ...")
+  (let-values ([(lexeme type paren start end) (racket-lexer in)])
+    (values lexeme 'keyword paren start end)))
+
+#;
+(define (protc-color port offset #;racket-coloring-mode?)
   (cond
-    [(let ([peek (peek-string 4 0 port)])
-       (or (equal? peek "impl")  ; FIXME ick
-           (equal? peek "spec")
-           (equal? peek "meas")
-           (equal? peek "actu")
-           (equal? peek "make")))
+    [(let ([peek (peek-string 9 0 port)])
+       (println peek)
+       (or (string-prefix? peek "spec")  ; FIXME ick
+           (string-prefix? peek "impl")
+           (string-prefix? peek "measure")
+           (string-prefix? peek "actualize")
+           (string-prefix? peek "make")))
      (define-values (str cat paren start end)
        (protc-lexer port))
-     ;(displayln (list 'color?: str) (current-output-port))
+     (displayln (list 'color?: str) (current-output-port))
      (define switch-to-racket-mode
        (let ([s0 (string-ref str 0)])
          (or (equal? s0 " ")  ; FIXME :/
+             (equal? s0 "\n")
+             (equal? s0 "\t")
              (equal? s0 "(")
              (equal? s0 "[")
              (equal? s0 "{"))))
-     (values str cat paren start end 0 switch-to-racket-mode)]
+     (values str cat paren start end 0 #;switch-to-racket-mode)]
     [else
      (define-values (str cat paren start end)
        (racket-lexer port))
-     (values str cat paren start end 0 #t)]))
+     (values str 'error paren start end 0 #;#t)]))
 
 (module+ test
-  (protc-color (open-input-string "impl") 0 #f)
-  (protc-color (open-input-string "spec") 0 #f)
-  (protc-color (open-input-string "make") 0 #f)
-  (protc-color (open-input-string "measure") 0 #f)
-  (protc-color (open-input-string "actualize") 0 #f)
-  (protc-color (open-input-string "(impl (a b c)) (spec (c d e))") 0 #f)
+  (require (for-syntax racket/base syntax/parse))
+  (define-syntax (values->list stx)
+    (syntax-parse stx
+      [(_ asdf:expr)
+       #'(call-with-values (λ () asdf) list)]))
+
+  (values->list (protc-color (open-input-string "a") 0))
+  (values->list (protc-color (open-input-string "aa") 0))
+  (values->list (protc-color (open-input-string "aaa") 0))
+
+  (values->list (protc-color (open-input-string "spec") 0))
+  (values->list (protc-color (open-input-string "impl") 0))
+
+  (values->list (protc-color (open-input-string "make") 0))
+  (values->list (protc-color (open-input-string "make") 0))
+  (values->list (protc-color (open-input-string "measure") 0))
+  (values->list (protc-color (open-input-string "actualize") 0))
+
+  (values->list (protc-color (open-input-string " impl") 0))
+  (values->list (protc-color (open-input-string "impl ") 0))
+  (values->list (protc-color (open-input-string "mpl (") 0))
+  (values->list (protc-color (open-input-string "pl (m") 0))
+  (values->list (protc-color (open-input-string "l (ma") 0))
+  (values->list (protc-color (open-input-string " (mak") 0))
+
+  (values->list (protc-color (open-input-string "impl ") 0))
+  (values->list (protc-color (open-input-string "implement") 0))
+
+  (values->list (protc-color (open-input-string "(spec") 0))
+  (values->list (protc-color (open-input-string "(make") 0))
+
+  (values->list (protc-color (open-input-string "(impl (a b c)) (spec (c d e))") 0))
   )
