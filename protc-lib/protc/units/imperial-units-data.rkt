77d9f51d62bdbbed7cac3fbf528587d9e058c875
Tom Gillespie <tgbugs@gmail.com>
2019-02-24 14:17:15 -0800

    units added imperial inches and lb for pounds

--- protc-lib/protc/units/imperial-units-data.rkt
+++ protc-lib/protc/units/imperial-units-data.rkt
@@ -2,10 +2,12 @@
 (provide units-imp)
 (define units-imp
 '((ft . feet)
+  (in . inches)
   (slugs . slugs)
   (°F . degrees-fahrenheit)
   (~oF . degrees-fahrenheit) ; Tom also accepts using the digraph for the degree symbol...
   (ºF . degrees-fahrenheit) ; b'\xc2\xba'
   (◦F . degrees-fahrenheit) ; b'\xe2\x97\xa6' white dot
+  (lb . pounds)  ; FIXME normalization
   (lbs . pounds))
 )
