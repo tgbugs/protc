cb7368df619b5a50bd17e7d9fae92a5f3aadead8
Tom Gillespie <tgbugs@gmail.com>
2019-04-02 05:56:47 -0400

    setup extract version using regext to avoid import errors

--- protcur/setup.py
+++ protcur/setup.py
@@ -1,11 +1,23 @@
+import re
 from setuptools import setup
+
+
+def find_version(filename):
+    _version_re = re.compile(r"__version__ = '(.*)'")
+    for line in open(filename):
+        version_match = _version_re.match(line)
+        if version_match:
+            return version_match.group(1)
+
+
+__version__ = find_version('protcur/__init__.py')
 
 with open('README.org', 'rt') as f:
     # TODO pandoc conversion
     long_description = f.read()
 
 setup(name='protcur',
-      version='0.0.1',
+      version=__version__,
       description='A dashboard for web annotation workflows for protocol curation.',
       long_description=long_description,
       long_description_content_type='text/plain',
