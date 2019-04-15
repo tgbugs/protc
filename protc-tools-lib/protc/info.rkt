2c4519f949484a25db0b755f2241bd7b3f4c0c22
Tom Gillespie <tgbugs@gmail.com>
2019-04-15 01:21:31 -0700

    add test folder for protc/protocols
    renamed existing test protocol files

--- protc-tools-lib/protc/info.rkt
+++ protc-tools-lib/protc/info.rkt
@@ -2,11 +2,13 @@
 
 (define raco-commands
   (list '("protc" protc/cli "protc commands" #f)
-        #;(list "protc-check"
+        #;
+        (list "protc-check"
               'protc/check/cli
               "runs correctness and completeness checks for Protc protocols"
               #f)
-        #;(list "protc-export"  ; FIXME want protc export, protc check etc...
+        #;
+        (list "protc-export"  ; FIXME want protc export, protc check etc...
               'protc/export/cli
               "export Protc protocols to executable formats"
               #f)))
