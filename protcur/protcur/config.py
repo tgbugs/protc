3acfe53b698c800b8954ff95ef445fa2e651dcbc
Tom Gillespie <tgbugs@gmail.com>
2019-05-07 17:05:25 -0700

    protcur config manage paths to unit resources internally

--- protcur/protcur/config.py
+++ protcur/protcur/config.py
@@ -1,2 +1,5 @@
 from pathlib import Path
 __script_folder__ = Path(__file__).resolve().parent
+__units_folder__ = Path(__script_folder__, '../../protc-lib/protc/units')
+__units_test_folder__ = Path(__script_folder__, '../../protc-lib/protc/units/test')
+__units_test_params__ = Path(__script_folder__, '../../protc-lib/protc/units/test/params.rkt')
