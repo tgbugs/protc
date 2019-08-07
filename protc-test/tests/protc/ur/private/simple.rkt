dc276da6cf941bc60c62dae3af4c7c0459c2f093
Tom Gillespie <tgbugs@gmail.com>
2019-08-06 21:32:42 -0700

    tests for #lang protc/ur files

---
+++ protc-test/tests/protc/ur/private/simple.rkt
@@ -0,0 +1,5 @@
+#lang protc/ur
+
+(protc:black-box-component "interhemispheric sulcus" (hyp: 'lol)
+                           (protc:black-box-component "the cutting blade (implied)" (hyp: 'lol))
+                           (protc:aspect "angle" (hyp: 'another-annotation)))
