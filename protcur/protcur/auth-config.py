fe91218d1e2a51ac230b7525022ab9e7d43f8d6a
Tom Gillespie <tgbugs@gmail.com>
2020-05-01 13:32:46 -0700

    protcur auth-config add hypothesis-* variables

--- protcur/protcur/auth-config.py
+++ protcur/protcur/auth-config.py
@@ -7,8 +7,8 @@
                                '{:prefix}/share/protcur/units',  # system
                                '/usr/share/protcur/units',  # pypy3
                                '{:cwd}/share/protcur/units',  # ebuild testing
-                              ],
-                  },
+                   ],
+ },
   'tags-folder-file': {'environment-variables': 'PROTCUR_TAGS_FOLDER_FILE',  # NOTE you must provide path to the test file
                        'default': ['../../protc-tags.rkt',  # git
                                    '../resources/protc-tags.rkt',  # test sdist
@@ -16,7 +16,10 @@
                                    '{:prefix}/share/protcur/protc-tags.rkt',  # system
                                    '/usr/share/protcur/protc-tags.rkt',  # pypy3
                                    '{:cwd}/share/protcur/protc-tags.rkt',  # ebuild testing
-                                  ],
-                      },
+                       ],
+  },
+  'hypothesis-api-key': {'environment-variables': 'PROTCUR_HYP_API_KEY HYP_API_KEY HYP_API_TOKEN'},
+  'hypothesis-group': {'environment-variables': 'PROTCUR_HYP_GROUP HYP_GROUP'},
+  'hypothesis-user': {'environment-variables': 'PROTCUR_HYP_USER HYP_USER'},
  },
 }
